1c80bc97ee0fda2856f5adf9ed734ec30a883ab3
25133656 <boliveiraassisreis@gmail.com>
2026-04-01 11:28:37 -0300

    feat:Introdução do projeto lazy

---
+++ app/src/main/java/com/aphamogged/listlazy/MainActivity.kt
@@ -0,0 +1,171 @@
+package com.aphamogged.listlazy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aphamogged.listlazy.model.Game
+import com.aphamogged.listlazy.repository.gameByStudios
+import com.aphamogged.listlazy.repository.getAllGames
+import com.aphamogged.listlazy.ui.theme.ListLazyTheme
+import kotlin.math.sign
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ListLazyTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GameScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GameScreen(modifier: Modifier = Modifier) {
+    var studio by remember {
+        mutableStateOf("")
+    }
+
+    var gameList by remember {
+        mutableStateOf(getAllGames())
+    }
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "Meus jogos favoritos",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        OutlinedTextField(
+            value = studio,
+            onValueChange = {
+                studio = it
+            },
+            modifier.fillMaxWidth(),
+            label = {Text(text = "Nome estúdio")},
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        gameList = gameByStudios(studio)
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Ícone de busca"
+                    )
+                }
+            }
+        )
+        LazyRow() {
+            items(gameList) {
+                StudioCard(it)
+            }
+        }
+        LazyColumn () {
+            items(gameList) {
+                GameCard(it)
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun GameCard(game: Game) {
+    Card(modifier = Modifier.padding(bottom = 8.dp)) {
+        Row( modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column( modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .weight(3f)
+            ) {
+                Text(
+                    text = game.title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = game.studio,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                text = "${game.releaseYear}",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
+        }
+    }
+}
+
+@Composable
+fun StudioCard(game: Game) {
+    Card(
+        modifier = Modifier.size(150.dp)
+            .padding(4.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = game.studio,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/aphamogged/listlazy/model/Game.kt
@@ -0,0 +1,9 @@
+package com.aphamogged.listlazy.model
+
+data class Game (
+    val id: Long = 0,
+    val title : String= "",
+    val releaseYear: Int = 0,
+    val studio : String = ""
+
+    )
